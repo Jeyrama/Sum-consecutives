c00af84a5564e84cf2962de6f61ed3117980845e
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-06-16 21:56:26 -0700

    Update Sum-consecutives.js

--- Sum-consecutives.js
+++ Sum-consecutives.js
@@ -23,3 +23,11 @@ Same meaning: 1 == 1
 
 
 // Solution
+
+function sumConsecutives(s) {
+  return s.reduce(function(prev, curr, i, arr) {
+    if(curr != arr[i - 1]) prev.push(curr);
+    else prev[prev.length - 1] += curr;
+    return prev;
+  }, []);
+}
